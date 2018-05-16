b7a51e8ada3ebf6678a873735e74c2962754708a
ashi-kakkar20 <38372085+ashi-kakkar20@users.noreply.github.com>
2018-05-16 22:41:53 +0530

    Update assignment8.3.js

--- assignment8.3.js
+++ assignment8.3.js
@@ -9,7 +9,7 @@ function printTimesTable(num)
     for(i=1;i<=10;i++)
     {
         res= i * num  ;
-        console.log(num + "*" + i + " = " + res + "" )
+        console.log(num + " * " + i + " = " + res + "" )
     }
  }
 
